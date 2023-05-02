ca191fd9e3281b118b196bc495e4a180970f4871
Ashutosh Sharma <ashutosh.sharma068@gmail.com>
2023-05-02 12:58:38 +0530

    graphql data log chnages

--- pages/graphExample.tsx
+++ pages/graphExample.tsx
@@ -4,7 +4,7 @@ import { useQuery } from "@apollo/client"
 
 const GraphExample: NextPage = () => {
     const { loading, error: subgraphQueryError, data } = useQuery(GET_ACTIVE_ITEMS)
-    console.log(data)
-    return <div>Hi :) </div>
+    // console.log("Data: ")
+    return <div>Data: {JSON.stringify(data, null, 2)}</div>
 }
 export default GraphExample
